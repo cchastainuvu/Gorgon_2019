feded5d8aa6345cdadb26dcdc867cfd6f8a7b96b
Unknown <10798368@my.uvu.edu>
2019-05-14 02:02:58 -0600

    project setup, checkers exercise pt. 1

---
+++ Gorgon_Summer19/Assets/Scripts/Checkers/Checkers.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkers : MonoBehaviour 
+{
+    public Piece [,] pieces = new Piece[8,8];
+
+    public GameObject blackPiece, whitePiece;
+    
+    private Vector3 _boardOffset = new Vector3(-6.5F, 0F, -3F);
+    private Vector3 _pieceOffset = new Vector3(0.5F, 0F, 0.5F);
+
+
+    private void Start()
+    {
+        GenerateBoard();
+    }
+    
+    private void GenerateBoard()
+    {
+        //Generate White Team
+
+        // y = 0 sets the starting position.
+        for (int y = 0; y < 3; y++)
+        {
+            bool oddRow = (y % 2 == 0);
+            
+            for (int x = 0; x < 8; x += 2)
+            {
+                //Generate Piece
+                GeneratePiece((oddRow)? x : x+1 ,y);
+            }
+        }
+        
+        //Generate Black Team
+        
+        for (int y = 7; y > 4; y--)
+        {
+            bool oddRow = (y % 2 == 0);
+            
+            for (int x = 0; x < 8; x += 2)
+            {
+                //Generate Piece
+                GeneratePiece((oddRow)? x : x+1 ,y);
+            }
+        }
+    }
+
+
+    private void GeneratePiece(int x, int y)
+    {
+
+        bool IsWhite = (y < 3) ? false : true;
+        
+        GameObject obj = Instantiate((IsWhite) ? whitePiece : blackPiece) as GameObject;
+        obj.transform.SetParent(transform);
+        Piece p = obj.GetComponent<Piece>();
+
+        pieces[x, y] = p;
+        
+        MovePiece(p,x,y);
+    }
+
+
+    private void MovePiece(Piece p, int x, int y)
+    {
+        p.transform.position = (Vector3.right * x) + (Vector3.forward * y) + _boardOffset + _pieceOffset;
+    }
+}
---
+++ Gorgon_Summer19/Assets/Scripts/SpawnToGrid.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnToGrid : MonoBehaviour 
+{
+	private ObjectToSpawn [,] _objects = new ObjectToSpawn[8,8];
+
+	public GameObject Spawn01, Spawn02;
+	//Create as many Spawns as desired.
+	
+	private Vector3 _offset1 = new Vector3(4F,0F,4F);
+	private Vector3 _offset2 = new Vector3(1F,0F,1F);
+
+	private void Start()
+	{
+		GenerateSpawn();
+	}
+
+	private void GenerateSpawn()
+	{
+		//Generate Spawn01 - Action must be repeated for other spawned objects.
+
+		for (int y = 0; y < 3; y++)
+		{
+			for (int x = 0; x < 8; x += 2)
+			{
+				PlaceSpawn(x,y);
+			}
+		}	
+	}
+
+	private void PlaceSpawn(int x, int y)
+	{
+		GameObject obj = Instantiate(Spawn01) as GameObject;
+		obj.transform.SetParent(transform);
+
+		ObjectToSpawn spawn = obj.GetComponent<ObjectToSpawn>();
+
+		_objects[x, y] = spawn;
+		
+		MoveSpawn(spawn, x, y);
+	}
+
+	private void MoveSpawn(ObjectToSpawn spawn, int x, int y)
+	{
+		spawn.transform.position = (Vector3.right * x) + (Vector3.forward * y) + _offset1 + _offset2;
+	}
+}
