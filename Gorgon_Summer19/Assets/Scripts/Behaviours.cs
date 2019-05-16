0006ff50b3ceeb3c8141b2592d3a056594178646
Unknown <10798368@my.uvu.edu>
2019-05-15 23:19:47 -0600

    unity lighting exercise/lecture

---
+++ Gorgon_Summer19/Assets/Scripts/Behaviours.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Behaviours : MonoBehaviour
+{
+    public UnityEvent OnAwake, OnStart, MouseDown, MouseEnter, MouseExit,
+        MouseRight, TriggerEnter, TriggerExit, CollisionEnter, DestroyEvent;
+    
+    
+    void Awake()
+    {
+        OnAwake.Invoke();
+    }
+    
+    void Start()
+    {
+        OnStart.Invoke();
+    }
+    
+    private void OnMouseDown()
+    {
+        MouseDown.Invoke();
+    }
+
+    private void OnMouseEnter()
+    {
+        MouseEnter.Invoke();
+    }
+
+    private void OnMouseExit()
+    {
+        MouseExit.Invoke();
+    }
+
+    private void RightClick()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            MouseRight.Invoke();
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        TriggerEnter.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        TriggerExit.Invoke();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        CollisionEnter.Invoke();
+    }
+
+    private void OnDestroy()
+    {
+        DestroyEvent.Invoke();
+    }
+
+}
---
+++ Gorgon_Summer19/Assets/Scripts/InstantiateTransform.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateTransform : MonoBehaviour
+{
+    public Transform Obj, Space;
+
+    public void PerformInstantiate()
+    {
+      Instantiate(Obj, Space.position, Space.transform.rotation);
+    }
+}
