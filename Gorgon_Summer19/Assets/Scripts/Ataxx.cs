1ac05364eeab0930ff63d683357ebaee78f9c2d1
Unknown <10798368@my.uvu.edu>
2019-05-29 23:44:36 -0600

    new player prefab setup

---
+++ Gorgon_Summer19/Assets/Scripts/Ataxx.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ataxx : MonoBehaviour
+{
+    private Vector2 _mouseOver, _startDrag, _endDrag;
+
+    private void Update()
+    {
+        TrackInput();   
+    }
+
+    private void TrackInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (gameObject.CompareTag("Close") || gameObject.CompareTag("Distant"))
+            {
+                
+            }
+        }
+    }
+}
